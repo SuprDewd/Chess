21cbb11f89c18da1f349993d59797202bfccda2d
SuprDewd <suprdewd@gmail.com>
2010-10-24 22:46:33 +0000

    Update.

--- ChessLib/ChessLib/Misc.cs
+++ ChessLib/ChessLib/Misc.cs
@@ -23,5 +23,54 @@ namespace ChessLib
                 action(obj);
             }
         }
+
+        /// <summary>
+        /// Unions all elements in the current instance and the specified collection.
+        /// </summary>
+        /// <typeparam name="T">The type of the collections.</typeparam>
+        /// <param name="source">The current instance.</param>
+        /// <param name="other">The collection to union.</param>
+        /// <returns>The current instance and the specified collection unioned.</returns>
+        public static IEnumerable<T> UnionAll<T>(this IEnumerable<T> source, IEnumerable<T> other)
+        {
+            foreach (T item in source)
+            {
+                yield return item;
+            }
+
+            foreach (T item in other)
+            {
+                yield return item;
+            }
+        }
+
+        /// <summary>
+        /// Returns all items in the current instance, until the predicate returns false, and then returns one more item.
+        /// </summary>
+        /// <typeparam name="T">The type of the current instance.</typeparam>
+        /// <param name="source">The current instance.</param>
+        /// <param name="predicate">The predicate to apply on all items.</param>
+        /// <returns>The items.</returns>
+        public static IEnumerable<T> TakeWhileAndOneMore<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            bool oneMore = true;
+
+            foreach (T item in source)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+                else if (oneMore)
+                {
+                    yield return item;
+                    oneMore = false;
+                }
+                else
+                {
+                    yield break;
+                }
+            }
+        }
     }
 }
