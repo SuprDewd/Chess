d35ab1e221e13098383db11de9a0b1da2e72e743
SuprDewd <suprdewd@gmail.com>
2010-10-26 10:52:44 +0000

    Added movement behaviour.

---
+++ ChessLib/ChessLib/MovementBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChessLib
+{
+    /// <summary>
+    /// An abstract class describing a movement behaviour.
+    /// </summary>
+    public abstract class MovementBehaviour : BelongsTo<ChessBoard, ChessPiece>
+    {
+        /// <summary>
+        /// The constructor.
+        /// </summary>
+        /// <param name="chessPiece">The Chess piece that the movement behaviour belongs to is on.</param>
+        public MovementBehaviour(ChessPiece chessPiece)
+            : base(chessPiece.Board, chessPiece)
+        {
+
+        }
+
+        /// <summary>
+        /// Moves the Chess piece.
+        /// </summary>
+        /// <param name="s">The square to move the Chess piece to.</param>
+        /// <returns>Whether the move was successful.</returns>
+        public abstract bool Move(Square s);
+    }
+}
