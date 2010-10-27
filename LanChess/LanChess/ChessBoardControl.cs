9cf94cf5be35a44411f001b731256ec98b9e3602
SuprDewd <suprdewd@gmail.com>
2010-10-27 17:19:15 +0000

    Started working on internet Chess.

---
+++ LanChess/LanChess/ChessBoardControl.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Media;
+using ChessLib;
+using System.Windows;
+
+namespace LanChess
+{
+    /// <summary>
+    /// A Chess board control.
+    /// </summary>
+    public class ChessBoardControl : Grid, IChessItem
+    {
+        private ChessBoard _Board = null;
+
+        /// <summary>
+        /// The Chess board the control is representing.
+        /// </summary>
+        public ChessBoard Board { get { return this._Board; } set { this._Board = value; this.Initialize(); } }
+        /// <summary>
+        /// The location of the Chess piece images.
+        /// </summary>
+        public Uri ImageDirectory { get; set; }
+
+        /// <summary>
+        /// An empty constructor.
+        /// </summary>
+        public ChessBoardControl() { }
+
+        /// <summary>
+        /// The constructor.
+        /// </summary>
+        /// <param name="board">The Chess board the control is representing.</param>
+        public ChessBoardControl(ChessBoard board)
+        {
+            this.Board = board;
+        }
+
+        /// <summary>
+        /// The squares on the board.
+        /// </summary>
+        public ChessSquareControl[,] Squares { get; private set; }
+
+        /// <summary>
+        /// Initialize the board.
+        /// </summary>
+        private void Initialize()
+        {
+            this.Squares = new ChessSquareControl[8, 8];
+
+            for (int rank = 0; rank < 8; rank++)
+            {
+                this.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) });
+
+                Grid g = new Grid();
+                Grid.SetRow(g, rank);
+
+                for (int file = 0; file < 8; file++)
+                {
+                    g.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+
+                    ChessSquareControl sq = new ChessSquareControl(this, this.Board[rank + 1, file + 1]);
+
+                    Grid.SetColumn(sq, file);
+                    g.Children.Add(sq);
+                }
+
+                this.Children.Add(g);
+            }
+
+            this.Reset();
+        }
+
+        /// <summary>
+        /// Resets the control.
+        /// </summary>
+        private void Reset()
+        {
+
+        }
+
+        /*
+
+        /// <summary>
+        /// The size of the board.
+        /// </summary>
+        public double Size
+        {
+            get { return base.Height; }
+            set
+            {
+                base.Height = value;
+                base.Width = value;
+            }
+        }
+
+        /// <summary>
+        /// The height of the board.
+        /// </summary>
+        /// <remarks>This should not be used.</remarks>
+        private new double Height
+        {
+            get { return base.Height; }
+            set
+            {
+                this.Size = value;
+            }
+        }
+
+        /// <summary>
+        /// The width of the board.
+        /// </summary>
+        /// <remarks>This should not be used.</remarks>
+        private new double Width
+        {
+            get { return base.Width; }
+            set
+            {
+                this.Size = value;
+            }
+        }
+         
+         */
+    }
+}
---
+++ LanChess/LanChess/ChessSquareControl.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Media;
+using ChessLib;
+
+namespace LanChess
+{
+    /// <summary>
+    /// A Chess square control.
+    /// </summary>
+    public class ChessSquareControl : Canvas
+    {
+        private static Brush _DarkBrush = Brushes.Black;
+        private static Brush _LightBrush = Brushes.White;
+
+        /// <summary>
+        /// The brush to use if the current square is dark.
+        /// </summary>
+        public static Brush DarkBrush { get { return _DarkBrush; } set { _DarkBrush = value; } }
+        /// <summary>
+        /// The brush to use if the current square is light.
+        /// </summary>
+        public static Brush LightBrush { get { return _LightBrush; } set { _LightBrush = value; } }
+        /// <summary>
+        /// The square that the control is representing.
+        /// </summary>
+        public Square Square { get; private set; }
+        /// <summary>
+        /// The Chess board control the square control belongs to.
+        /// </summary>
+        public ChessBoardControl BoardControl { get; private set; }
+
+        /// <summary>
+        /// The constructor.
+        /// </summary>
+        /// <param name="background">The background of the square.</param>
+        public ChessSquareControl(ChessBoardControl boardControl, Square square)
+        {
+            this.BoardControl = boardControl;
+            this.Square = square;
+            this.Background = this.Square.Color;
+        }
+
+        /// <summary>
+        /// The background of the Chess square.
+        /// </summary>
+        private new ChessColor Background
+        {
+            get
+            {
+                return base.Background == ChessSquareControl.DarkBrush ? ChessColor.Black : ChessColor.White;
+            }
+            set
+            {
+                base.Background = value == ChessColor.Black ? ChessSquareControl.DarkBrush : ChessSquareControl.LightBrush;
+            }
+        }
+
+        private void Update()
+        {
+            this.Children.Clear();
+
+            // Change image and stuff.
+        }
+
+        /*
+
+        /// <summary>
+        /// The size of the square.
+        /// </summary>
+        public double Size
+        {
+            get { return base.Height; }
+            set
+            {
+                base.Height = value;
+                base.Width = value;
+            }
+        }
+
+        /// <summary>
+        /// The height of the square.
+        /// </summary>
+        /// <remarks>This should not be used.</remarks>
+        private new double Height
+        {
+            get { return base.Height; }
+            set
+            {
+                this.Size = value;
+            }
+        }
+
+        /// <summary>
+        /// The width of the square.
+        /// </summary>
+        /// <remarks>This should not be used.</remarks>
+        private new double Width
+        {
+            get { return base.Width; }
+            set
+            {
+                this.Size = value;
+            }
+        }
+         
+         */
+
+    }
+}
