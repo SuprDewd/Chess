a73adbdede6234d9729f47dd66db76650c812fcc
SuprDewd <suprdewd@gmail.com>
2010-11-05 14:04:38 +0000

    Started working on the Chess explainer.

---
+++ ChessExplainer/ChessExplainer/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChessExplainer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.cbcBoard.Board.GameEnded += (b, r) =>
+                {
+                    MessageBox.Show(r.ToString());
+                };
+        }
+
+        private void Grid_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            this.cbcBoard.Size = Math.Min(this.MainGrid.ActualHeight, this.MainGrid.ActualWidth - 150);
+        }
+    }
+}
