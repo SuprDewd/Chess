0c0633ed9391268b00573ccf6446941e6d1b5bd2
SuprDewd <suprdewd@gmail.com>
2010-11-08 15:25:41 +0000

    ChessServer update.

---
+++ ChessServer/ChessServer/ChessServer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpBag.Net;
+using SharpBag;
+using SharpBag.Logging;
+using System.Net;
+using System.IO;
+using System.Net.Sockets;
+
+namespace ChessServer
+{
+    public class ChessServer : IDisposable
+    {
+        private TcpServer Server { get; set; }
+        private List<TcpClientHandler> Clients { get; set; }
+        private Logger Logger { get; set; }
+
+        public ChessServer(IEnumerable<int> ports, Logger logger)
+        {
+            this.Logger = logger;
+            this.Clients = new List<TcpClientHandler>();
+            this.Server = TcpServer.Create(ports);
+            this.Server.ClientReceived += new Action<TcpServer, TcpClientHandler>(ClientReceived);
+
+            this.Logger.Log("Chess server started listening on " + this.Server.Listener.Server.RemoteEndPoint.ToString() + ".");
+        }
+
+        private void ClientReceived(TcpServer server, TcpClientHandler client)
+        {
+            this.Logger.Log("Client connected from " + client.Client.Client.RemoteEndPoint.ToString() + ".");
+
+            client.MessageReceived += new Action<TcpClientHandler, string>(MessageReceived);
+        }
+
+        private void MessageReceived(TcpClientHandler client, string message)
+        {
+            this.Logger.Log("Received message: " + message);
+
+            // TODO: 
+        }
+
+        public void Dispose()
+        {
+            this.Server.Dispose();
+        }
+    }
+}
---
+++ ChessServer/ChessServer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpBag;
+using SharpBag.Net;
+using SharpBag.IO;
+using SharpBag.Logging;
+using ChessLib;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ChessServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            InteractiveConsole IConsole = new InteractiveConsole();
+            Logger logger = new Logger(s => IConsole.WriteLine(s));
+
+            using (ChessServer server = new ChessServer(1337.To(13337), logger))
+            {
+
+            }
+        }
+    }
+}
