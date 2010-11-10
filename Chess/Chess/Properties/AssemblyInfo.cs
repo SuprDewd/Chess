d239382bc689afa58ef6212c7e2be7f5eef712d2
SuprDewd <suprdewd@gmail.com>
2010-11-10 12:21:16 +0000

    Cleanup.

--- Chess/Chess/Properties/AssemblyInfo.cs
+++ Chess/Chess/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ChessClient")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Chess")]
+[assembly: AssemblyDescription("Chess Software")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("ChessClient")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCompany("SuprDewd")]
+[assembly: AssemblyProduct("Chess")]
+[assembly: AssemblyCopyright("Copyright © SuprDewd 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
